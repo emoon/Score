867996981839a2b801a6dc0d604e2ba3e2e8990c
Daniel Collin <daniel@collin.com>
2011-04-02 23:46:00 +0200

    Fixed various compile errors

--- src/target/ps2/ee/socket.h
+++ src/target/ps2/ee/socket.h
@@ -3,14 +3,18 @@
 #ifndef SCORE_SOCKET_H
 #define SCORE_SOCKET_H
 
+#include "types.h"
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 struct Socket;
 struct Address;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-struct Socket* Socket_createTcp(const Address* address);
-struct Socket* Socket_accept(Socket* socket);
-struct Socket* Socket_connect(const Address* address);
+struct Socket* Socket_createTcp(const struct Address* address);
+struct Socket* Socket_accept(struct Socket* socket);
+struct Socket* Socket_connect(const struct Address* address);
 
 bool Socket_setBlocking(struct Socket* socket, bool blocking);
 bool Socket_listen(struct Socket* socket, unsigned int backlog);
@@ -20,7 +24,7 @@ bool Socket_send(struct Socket* socket, const void* buffer, unsigned int length)
 int Socket_receive(struct Socket* socket, void* buffer, unsigned int length);
 
 bool Socket_stringToAddress(struct Address* address, const char* string);
-bool Socket_nameToAddressPort(struct Address* address, const char* hostname, uint16_t port);
+bool Socket_nameToAddressPort(struct Address** address, const char* hostname, uint16_t port);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
