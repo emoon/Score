e941a7604bbb7bb948b809f683c849e1e878fc75
Daniel Collin <daniel@collin.com>
2011-04-02 23:46:23 +0200

    Added paths and libs needed to build the code

--- src/target/ps2/units.lua
+++ src/target/ps2/units.lua
@@ -1,13 +1,26 @@
 Program {
 	Name = "score_ee_target",
 	Sources = {
+		-- tinylibc
+		"ee/libc/init.c",
+		"ee/libc/sbrk.c",
+		"ee/libc/string/memcpy.c",
+		"ee/libc/string/memset.c",
+		"ee/libc/string/strlen.c",
+		"ee/libc/string/sprintf.c",
+		"ee/libc/string/ctype.c",
+		"ee/libc/stdio/printf.c",
+		"ee/libc/stdlib/abort.c",
+		"ee/libc/stdlib/init.c",
+		-- main code
 		"ee/score_connection.c",
 		"ee/socket.c",
 		"ee/target_ps2_ee.c",
 	},
 
 	Libs = {
-		{ "c", "gcc", "debug", "kernel", "syscall", "ps2snd", "pad", "m", "fileXio"; Config = "ps2-*-*-*" },
+		-- { "c", "gcc", "debug", "kernel", "syscall", "ps2snd", "pad", "m", "fileXio"; Config = "ps2-*-*-*" },
+		{ "gcc", "scf", "ent_smap", "eenet", "eenetctl", "graph", "kernl", "m", "sn", "sneetcp"; Config = "ps2-*-*-*" },
 	}
 	
 }
