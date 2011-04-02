326a36c4296b4433508f1749c69b6413810df2f9
Daniel Collin <daniel@collin.com>
2011-04-02 23:45:41 +0200

    Added types

---
+++ src/target/ps2/ee/types.h
@@ -0,0 +1,25 @@
+
+#ifndef SCORE_TYPES_H
+#define SCORE_TYPES_H
+
+typedef unsigned char uint8_t;
+typedef signed char int8_t;
+
+typedef unsigned short uint16_t;
+typedef signed short int16_t;
+
+typedef unsigned int uint32_t;
+typedef signed int int32_t;
+
+typedef unsigned char bool;
+
+#ifndef true
+#define true	1
+#endif
+
+#ifndef false
+#define false	0
+#endif
+
+#endif
+
