c1feda05f87808c641cbf3533ca57494a8622776
Daniel Collin <daniel@collin.com>
2011-04-03 23:43:21 +0200

    Setup connection (with the recent changes it's now possible to run simple code on the target, yah!)

--- src/target/ps2/ee/target_ps2_ee.c
+++ src/target/ps2/ee/target_ps2_ee.c
@@ -2,8 +2,20 @@
 //
 //
 
+#include "score_connection.h"
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 int main()
 {
+	ScoreConnection_create();
+	
+	while (1)
+	{
+		//ScoreConnection_update();
+		ScoreConnection_safePoint();
+	}
+
 	return 0;
 }
 
